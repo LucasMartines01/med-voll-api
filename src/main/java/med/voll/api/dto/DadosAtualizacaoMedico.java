2bd34a16d5d813334c692177f458f5f216b1feb3
LucasMartines01 <lucas.martines@izio.com.br>
2023-05-14 14:23:01 -0300

    modulo de médicos

---
+++ src/main/java/med/voll/api/dto/DadosAtualizacaoMedico.java
@@ -0,0 +1,7 @@
+package med.voll.api.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoMedico(@NotNull Long id, String nome, String telefone, @Valid DadosEndereco endereco) {
+}
---
+++ src/main/java/med/voll/api/dto/ListagemMedico.java
@@ -0,0 +1,9 @@
+package med.voll.api.dto;
+
+import med.voll.api.models.Medico;
+
+public record ListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
+    public ListagemMedico (Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
