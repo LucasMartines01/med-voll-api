3e80ca6be59a54c831453ed8e60924367c4424b3
LucasMartines01 <lucas.martines@sptech.school>
2023-05-16 12:07:18 -0300

    jwt and spring security

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,33 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.config.security.TokenJWT;
+import med.voll.api.config.security.TokenService;
+import med.voll.api.domain.dto.DadosAutenticacao;
+import med.voll.api.domain.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autenticar")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity autenticar(@RequestBody @Valid DadosAutenticacao dados){
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+        var token = tokenService.generateToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok().body(new TokenJWT(token));
+    }
+}
