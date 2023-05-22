1e4bf90494c310027695bd35f450ab1e9913e4ea
LucasMartines01 <lucas.martines@izio.com.br>
2023-05-22 12:18:13 -0300

    documentação api

--- src/main/java/med/voll/api/controller/ConsultasController.java
+++ src/main/java/med/voll/api/controller/ConsultasController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.dto.consultas.DadosAgendamentoConsulta;
 import med.voll.api.domain.dto.consultas.DadosDetalhamentoConsulta;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultasController {
     @Autowired
     ConsultasService service;
